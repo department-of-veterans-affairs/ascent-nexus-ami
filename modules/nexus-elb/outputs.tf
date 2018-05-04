c922b91fa9eb2c74c7e27371963830c8b6ee7f3d
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-05-04 08:31:06 -0500

    PLAT-169: fix syntax issue; add an output

--- modules/nexus-elb/outputs.tf
+++ modules/nexus-elb/outputs.tf
@@ -1,3 +1,7 @@
 output "security_group_id" {
-  value = "module.elb_security_group.security_group_id"
+  value = "${module.elb_security_group.security_group_id}"
+}
+
+output "elb_dns" {
+  value = "${aws_elb.nexus.zone_id}"
 }
