c911f47a14676c6c542be057cd8c779c31bd6db1
jasonluck <jluck@meetveracity.com>
2018-06-15 12:47:35 -0400

    updating JDK to the latest 8 version

--- packer/install-java.sh
+++ packer/install-java.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # Install Java Runtime
-curl -L -H "Cookie: oraclelicense=accept-securebackup-cookie" -o jdk1.8.0_161.tar.gz http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz
-tar -xzf jdk1.8.0_161.tar.gz
-rm jdk1.8.0_161.tar.gz
+curl -L -H "Cookie: oraclelicense=accept-securebackup-cookie" -o jdk1.8.0_171.tar.gz http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-i586.tar.gz
+tar -xzf jdk1.8.0_171.tar.gz
+rm jdk1.8.0_171.tar.gz
 sudo mkdir -p /usr/lib/jvm
-sudo mv jdk1.8.0_161 /usr/lib/jvm/jdk1.8.0_161
-sudo chown -R root:root /usr/lib/jvm/jdk1.8.0_161
+sudo mv jdk1.8.0_171 /usr/lib/jvm/jdk1.8.0_171
+sudo chown -R root:root /usr/lib/jvm/jdk1.8.0_171
 
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.8.0_161/bin/java 1200
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.8.0_171/bin/java 1200
